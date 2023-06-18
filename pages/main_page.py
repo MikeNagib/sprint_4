a6c88e24f09bcc51a2c3804ccd50c34b0fd74ada
MikeNagib <132690732+MikeNagib@users.noreply.github.com>
2023-06-18 22:09:54 +0300

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,7 +3,7 @@ from locators.questions_page_locators import QuestionsPageLocators
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators.main_page_locators import MainPageLocators
-from data import link_main_page, TIME_TO_WAIT_ELEMENTS, link_dzen
+from data import  TIME_TO_WAIT_ELEMENTS, link_dzen
 from pages.base_page import BasePage
 
 
@@ -13,18 +13,25 @@ class MainPage(BasePage):
     def accept_cookies(self):
         self.driver.find_element(*MainPageLocators.get_cookie_button).click()
 
-    @allure.step('Переход на Яндекс лого')
-    def verify_if_link_to_yandex_works(self):
-        self.driver.get(link_main_page)
-        return self.navigate_to_link(MainPageLocators.logo_yandex, link_dzen)
+    @allure.step('Клик на лого Яндекс')
+    def click_to_yandex_works(self):
+        self.navigate_to_link(MainPageLocators.logo_yandex, link_dzen)
 
+    @allure.step('Клик на лого самоката')
     def click_scooter_logo(self):
         self.driver.find_element(*MainPageLocators.logo_scooter).click()
 
     @allure.step('Найти ответ')
-    def find_answer(self, answer_locator):
+    def check_answer(self, answer_locator):
         WebDriverWait(self.driver, TIME_TO_WAIT_ELEMENTS).until(
             expected_conditions.visibility_of_element_located(QuestionsPageLocators.open_response_text))
         assert self.driver.find_element(*QuestionsPageLocators.open_response_text).text == \
                answer_locator[1].split('\'')[
                    1]
+
+    @allure.step('Найти вопрос')
+    def find_question(self, question_locator):
+        self.driver.execute_script("arguments[0].scrollIntoView();", self.driver.find_element(*question_locator))
+        WebDriverWait(self.driver, TIME_TO_WAIT_ELEMENTS).until(
+            expected_conditions.element_to_be_clickable(question_locator))
+        self.driver.find_element(*question_locator).click()
