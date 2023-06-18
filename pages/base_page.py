a1c8f1fa08ad6cd5720c35468fc517f7aba912f0
MikeNagib <132690732+MikeNagib@users.noreply.github.com>
2023-06-18 22:09:34 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -30,16 +30,6 @@ class BasePage:
             expected_conditions.visibility_of_element_located(by_locator))
         return self.driver.find_element(*by_locator).text
 
-    def wait_element(self, locator):
-        WebDriverWait(self.driver, TIME_TO_WAIT_ELEMENTS).until(
-            expected_conditions.visibility_of_element_located(locator))
 
-    def wait_page_is_loaded(self):
-        self.wait_element(MainPageLocators.STATUS_BUTTON)
 
-    @allure.step('Найти вопрос')
-    def find_question(self, question_locator):
-        self.driver.execute_script("arguments[0].scrollIntoView();", self.driver.find_element(*question_locator))
-        WebDriverWait(self.driver, TIME_TO_WAIT_ELEMENTS).until(
-            expected_conditions.element_to_be_clickable(question_locator))
-        self.driver.find_element(*question_locator).click()
+
