ca846aa21ad0c818c68e174f4542f833ddddaf45
MikeNagib <132690732+MikeNagib@users.noreply.github.com>
2023-06-18 22:10:34 +0300

    Update test_logo.py

--- tests/test_logo.py
+++ tests/test_logo.py
@@ -1,18 +1,25 @@
 import allure
 from pages.main_page import MainPage
-from data import link_dzen, link_main_page
+from data import link_dzen, link_main_page, link_order
 
 
 @allure.suite('Тесты проверки логотипов')
 class TestClickLogo:
 
-    @allure.title('Проверка логотипа самоката')
-    def test_click_scooter_logo(self, driver):
-        page = MainPage(driver)
-        page.click_scooter_logo()
-        assert driver.current_url == link_main_page
 
     @allure.title('Проверка логотипа Яндекс дзен')
     def test_click_logo_dzen(self, driver):
         page = MainPage(driver)
-        assert link_dzen == page.verify_if_link_to_yandex_works()
+        driver.get(link_order)
+        page.click_to_yandex_works()
+        assert link_dzen == driver.current_url
+
+    @allure.title('Проверка логотипа самоката')
+    def test_click_scooter_logo(self, driver):
+        page = MainPage(driver)
+        driver.get(link_order)
+        page.click_scooter_logo()
+        assert driver.current_url == link_main_page
+
+
+
